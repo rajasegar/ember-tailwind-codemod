27959c89223a899f328dc12b1c292f6801604783
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-05-22 10:19:11 +0530

    chore: re-organize bin files to lib dir

--- bin/cli.js
+++ bin/cli.js
@@ -1,49 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-const globby = require('globby');
 const minimist = require('minimist');
-const workerpool = require('workerpool');
-const os = require('os');
-const chalk = require('chalk');
 
 const argv = minimist(process.argv.slice(2));
-const Logger = require('./logger');
-const StatsCollector = require('./stats-collector');
 
-const logger = new Logger();
-const stats = new StatsCollector(logger);
-
-const files = globby.sync(`${argv._}/**/*.hbs`, {
-  cwd: process.cwd(),
-  absolute: true,
-});
-
-const cpus = os.cpus().length;
-const processCount = Math.min(files.length, cpus);
-
-console.info(`Processing ${files.length} file${files.length !== 1 ? 's' : ''}…`);
-console.info(`Spawning ${processCount} worker${processCount !== 1 ? 's' : ''}…`);
-
-logger.spin('Processed 0 files');
-
-const pool = workerpool.pool(__dirname + '/worker.js');
-
-const output = files.map((filename, index) => {
-  return pool
-    .exec('transformFile', [filename, { css: argv.css }])
-    .then((result) => {
-      //console.log(result);
-      stats.update(result);
-      logger.updateSpinner(`Processed ${index + 1} files`);
-      return result;
-    })
-    .catch((err) => console.error(err));
-});
-
-Promise.all(output).then(() => {
-  pool.terminate();
-  logger.stopSpinner();
-  stats.print();
-  console.log('🎉', chalk.green('Migration completed'));
-});
+const runner = require('../lib/runner');
+runner(argv);
